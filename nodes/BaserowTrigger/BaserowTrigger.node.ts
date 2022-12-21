8841dce7dcb4cb69660b39b87e3f9fb3d7ce3236
Jonathan <jonathan.bennetts@gmail.com>
2022-12-21 20:12:54 +0000

    initial work commit

---
+++ nodes/BaserowTrigger/BaserowTrigger.node.ts
@@ -0,0 +1,225 @@
+import { IHookFunctions, IWebhookFunctions } from 'n8n-core';
+
+import {
+	IDataObject,
+	ILoadOptionsFunctions,
+	INodeType,
+	INodeTypeDescription,
+	IWebhookResponseData,
+	NodeApiError,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+import { baserowApiRequest, toOptions } from './GenericFunctions';
+import { LoadedResource } from './types';
+
+export class BaserowTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Baserow Trigger',
+		name: 'baserowTrigger',
+		icon: 'file:baserow.svg',
+		group: ['trigger'],
+		version: 1,
+		subtitle:
+			'={{$parameter["events"].join(", ")}}',
+		description: 'Starts the workflow when Baserow events occur',
+		defaults: {
+			name: 'Baserow Trigger',
+		},
+		inputs: [],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'baserowApi',
+				required: true,
+			},
+		],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			},
+		],
+		properties: [
+			{
+				displayName: 'Database Name or ID',
+				name: 'databaseId',
+				type: 'options',
+				default: '',
+				required: true,
+				description:
+					'Database to operate on. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+				typeOptions: {
+					loadOptionsMethod: 'getDatabaseIds',
+				},
+			},
+			{
+				displayName: 'Table Name or ID',
+				name: 'tableId',
+				type: 'options',
+				default: '',
+				required: true,
+				description:
+					'Table to operate on. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+				typeOptions: {
+					loadOptionsDependsOn: ['databaseId'],
+					loadOptionsMethod: 'getTableIds',
+				},
+			},
+			{
+				displayName: 'Events',
+				name: 'events',
+				type: 'multiOptions',
+				options: [
+					{
+						name: 'Rows Created',
+						value: 'rows.created',
+					},
+					{
+						name: 'Rows Deleted',
+						value: 'rows.deleted',
+					},
+					{
+						name: 'Rows Updated',
+						value: 'rows.updated',
+					},
+				],
+				required: true,
+				default: [],
+				description: 'The events to listen to',
+			},
+		],
+	};
+
+	methods = {
+		loadOptions: {
+			async getDatabaseIds(this: ILoadOptionsFunctions) {
+				const endpoint = '/api/applications/';
+				const databases = (await baserowApiRequest.call(
+					this,
+					'GET',
+					endpoint,
+				)) as LoadedResource[];
+				return toOptions(databases);
+			},
+
+			async getTableIds(this: ILoadOptionsFunctions) {
+				const databaseId = this.getNodeParameter('databaseId', 0) as string;
+				const endpoint = `/api/database/tables/database/${databaseId}/`;
+				const tables = (await baserowApiRequest.call(
+					this,
+					'GET',
+					endpoint,
+				)) as LoadedResource[];
+				return toOptions(tables);
+			},
+		},
+	}
+
+	// @ts-ignore (because of request)
+	webhookMethods = {
+		default: {
+			async checkExists(this: IHookFunctions): Promise<boolean> {
+				const webhookData = this.getWorkflowStaticData('node');
+
+				if (webhookData.webhookId === undefined) {
+					return false;
+				}
+				try {
+					await baserowApiRequest.call(this, 'GET', `/api/database/webhooks/${webhookData.webhookId}/`);
+				} catch (error) {
+					if (error.response.status === 404) {
+						delete webhookData.webhookId;
+						delete webhookData.webhookEvents;
+						return false;
+					}
+					throw error;
+				}
+				return true;
+			},
+			async create(this: IHookFunctions): Promise<boolean> {
+				const webhookUrl = this.getNodeWebhookUrl('default') as string;
+
+				if (webhookUrl.includes('//localhost')) {
+					throw new NodeOperationError(
+						this.getNode(),
+						'The Webhook can not work on "localhost". Please, either setup n8n on a custom domain or start with "--tunnel"!',
+					);
+				}
+
+				const tableId = this.getNodeParameter('tableId') as string;
+				const events = this.getNodeParameter('events', []);
+				const endpoint = `/api/database/webhooks/table/${tableId}/`;
+
+				const body = {
+					"url": webhookUrl,
+					"include_all_events": false,
+					events,
+					"request_method": "POST",
+					"name": `${this.getWorkflow().name}`,
+					"use_user_field_names": true
+				}
+
+				const webhookData = this.getWorkflowStaticData('node');
+
+				let responseData;
+				try {
+					responseData = await baserowApiRequest.call(this, 'POST', endpoint, body);
+				} catch (error) {
+					throw error;
+				}
+
+				if (responseData.id === undefined || responseData.active !== true) {
+					throw new NodeApiError(this.getNode(), responseData, {
+						message: 'Baserow webhook creation response did not contain the expected data.',
+					});
+				}
+
+				webhookData.webhookId = responseData.id as string;
+				webhookData.webhookEvents = responseData.events as string[];
+
+				return true;
+			},
+
+			async delete(this: IHookFunctions): Promise<boolean> {
+				const webhookData = this.getWorkflowStaticData('node');
+
+				if (webhookData.webhookId !== undefined) {
+					const endpoint = `/api/database/webhooks/${webhookData.webhookId}/`;
+					const body = {};
+					try {
+						await baserowApiRequest.call(this, 'DELETE', endpoint, body);
+					} catch (error) {
+						if (error.response.status !== 404) {
+							return false;
+						}
+					}
+					delete webhookData.webhookId;
+					delete webhookData.webhookEvents;
+				}
+				return true;
+			},
+		},
+	};
+
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		const bodyData = this.getBodyData();
+		if (bodyData.hook_id !== undefined && bodyData.action === undefined) {
+			return {
+				webhookResponse: 'OK',
+			};
+		}
+
+		const returnData: IDataObject[] = [];
+
+		returnData.push({
+			body: bodyData,
+		});
+
+		return {
+			workflowData: [this.helpers.returnJsonArray(bodyData)],
+		};
+	}
+}
---
+++ nodes/BaserowTrigger/GenericFunctions.ts
@@ -0,0 +1,155 @@
+import { IExecuteFunctions } from 'n8n-core';
+
+import { OptionsWithUri } from 'request';
+
+import { IDataObject, IHookFunctions, ILoadOptionsFunctions, NodeApiError } from 'n8n-workflow';
+
+import { Accumulator, BaserowCredentials, LoadedResource } from './types';
+
+/**
+ * Make a request to Baserow API.
+ */
+export async function baserowApiRequest(
+	this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions,
+	method: string,
+	endpoint: string,
+	//jwtToken: string,
+	body: IDataObject = {},
+	qs: IDataObject = {},
+) {
+	const credentials = (await this.getCredentials('baserowApi')) as BaserowCredentials;
+	const jwtToken = await getJwtToken.call(this, credentials);
+
+	const options: OptionsWithUri = {
+		headers: {
+			Authorization: `JWT ${jwtToken}`,
+		},
+		method,
+		body,
+		qs,
+		uri: `${credentials.host}${endpoint}`,
+		json: true,
+	};
+
+	if (Object.keys(qs).length === 0) {
+		delete options.qs;
+	}
+
+	if (Object.keys(body).length === 0) {
+		delete options.body;
+	}
+
+	try {
+		return this.helpers.request!(options);
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error);
+	}
+}
+
+/**
+ * Get a JWT token based on Baserow account username and password.
+ */
+export async function getJwtToken(
+	this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions,
+	{ username, password, host }: BaserowCredentials,
+) {
+	const options: OptionsWithUri = {
+		method: 'POST',
+		body: {
+			username,
+			password,
+		},
+		uri: `${host}/api/user/token-auth/`,
+		json: true,
+	};
+
+	try {
+		const { token } = (await this.helpers.request!(options)) as { token: string };
+		return token;
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error);
+	}
+}
+
+export async function getFieldNamesAndIds(
+	this: IExecuteFunctions,
+	tableId: string,
+	//jwtToken: string,
+) {
+	const endpoint = `/api/database/fields/table/${tableId}/`;
+	const response = (await baserowApiRequest.call(
+		this,
+		'GET',
+		endpoint,
+		//jwtToken,
+	)) as LoadedResource[];
+
+	return {
+		names: response.map((field) => field.name),
+		ids: response.map((field) => `field_${field.id}`),
+	};
+}
+
+export const toOptions = (items: LoadedResource[]) =>
+	items.map(({ name, id }) => ({ name, value: id }));
+
+/**
+ * Responsible for mapping field IDs `field_n` to names and vice versa.
+ */
+export class TableFieldMapper {
+	nameToIdMapping: Record<string, string> = {};
+
+	idToNameMapping: Record<string, string> = {};
+
+	mapIds = true;
+
+	async getTableFields(
+		this: IExecuteFunctions,
+		table: string,
+		//jwtToken: string,
+	): Promise<LoadedResource[]> {
+		const endpoint = `/api/database/fields/table/${table}/`;
+		return baserowApiRequest.call(this, 'GET', endpoint);
+	}
+
+	createMappings(tableFields: LoadedResource[]) {
+		this.nameToIdMapping = this.createNameToIdMapping(tableFields);
+		this.idToNameMapping = this.createIdToNameMapping(tableFields);
+	}
+
+	private createIdToNameMapping(responseData: LoadedResource[]) {
+		return responseData.reduce<Accumulator>((acc, cur) => {
+			acc[`field_${cur.id}`] = cur.name;
+			return acc;
+		}, {});
+	}
+
+	private createNameToIdMapping(responseData: LoadedResource[]) {
+		return responseData.reduce<Accumulator>((acc, cur) => {
+			acc[cur.name] = `field_${cur.id}`;
+			return acc;
+		}, {});
+	}
+
+	setField(field: string) {
+		return this.mapIds ? field : this.nameToIdMapping[field] ?? field;
+	}
+
+	idsToNames(obj: Record<string, unknown>) {
+		Object.entries(obj).forEach(([key, value]) => {
+			if (this.idToNameMapping[key] !== undefined) {
+				delete obj[key];
+				obj[this.idToNameMapping[key]] = value;
+			}
+		});
+	}
+
+	namesToIds(obj: Record<string, unknown>) {
+		Object.entries(obj).forEach(([key, value]) => {
+			if (this.nameToIdMapping[key] !== undefined) {
+				delete obj[key];
+				obj[this.nameToIdMapping[key]] = value;
+			}
+		});
+	}
+}
